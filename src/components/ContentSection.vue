08e04bca523f721b87f3cbb401a6851726145afd
OfficerYoda <officeryoda@gmail.com>
2025-06-19 22:11:32 +0200

    clickable chapters

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -1,6 +1,16 @@
 <template>
   <section class="content-section" :id="sectionId">
-    <h2 class="section-title">{{ title }}</h2>
+    <h2 class="section-title" @click="handleTitleClick" tabindex="0">
+      {{ title }}
+      <img
+        src="/link.svg"
+        class="section-indicator"
+        :class="{ 'clicked': indicatorClicked }"
+        alt=""
+        aria-hidden="true"
+        style="color: #535bf2;"
+      />
+    </h2>
     <div class="section-content">
       <slot></slot>
     </div>
@@ -8,7 +18,7 @@
 </template>
 
 <script lang="ts">
-import {computed, defineComponent} from 'vue';
+import {computed, defineComponent, ref} from 'vue';
 import {kebabUriCase} from "../utils/string.ts";
 
 export default defineComponent({
@@ -21,7 +31,17 @@ export default defineComponent({
   },
   setup(props) {
     const sectionId = computed(() => kebabUriCase(props.title));
-    return {sectionId};
+    const indicatorClicked = ref(false);
+
+    const handleTitleClick = () => {
+      // Change the URL to link to the chapter
+      window.location.hash = `#${sectionId.value}`;
+      indicatorClicked.value = true;
+      setTimeout(() => {
+        indicatorClicked.value = false;
+      }, 180); // Duration of the click effect in ms
+    };
+    return {sectionId, handleTitleClick, indicatorClicked};
   },
 });
 </script>
@@ -38,10 +58,41 @@ export default defineComponent({
 .section-title {
   color: #ffffff; /* Weißer Titel */
   font-size: 2.2rem;
+  margin-top: 0;
   margin-bottom: 1.5rem;
   border-bottom: 2px solid #333; /* Trennlinie unter dem Titel */
   padding-bottom: 0.8rem;
   letter-spacing: 0.05em;
+  display: flex;
+  justify-content: space-between;
+  transition: border-color 0.2s;
+}
+
+.section-indicator {
+  display: none;
+  margin-right: 0.7em;
+  margin-top: 1rem;
+  flex-shrink: 0;
+  align-items: center;
+  transition: opacity 0.2s, filter 0.15s;
+  width: 1em;
+  height: 1em;
+  /* Default filter */
+  filter: brightness(0) saturate(100%) invert(55%) sepia(0%) saturate(0%) hue-rotate(0deg) brightness(100%) contrast(100%);
+}
+
+.section-indicator.clicked {
+  /* Change filter on click */
+  filter: brightness(2) saturate(200%) invert(20%) sepia(80%) hue-rotate(220deg) contrast(150%);
+}
+
+.section-title:hover .section-indicator {
+  display: inline-flex;
+}
+
+.section-title:hover {
+  border-bottom: 2px solid #c2c2c2;
+  outline: none;
 }
 
 .section-content {
