384668de9240e5a9cc1fad32fe91580b1e2aea80
OfficerYoda <officeryoda@gmail.com>
2025-06-22 02:01:09 +0200

    Created ChapterTitle.vue

---
+++ src/components/ChapterTitle.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+defineProps<{
+  title: string,
+}>();
+</script>
+
+<template>
+  <h1 class="chapter-title">
+    {{ title }}
+  </h1>
+</template>
+
+<style scoped>
+.chapter-title {
+  background: linear-gradient(to bottom, #ffffff , #e0e0e0 65%, #1a1a1a 100%);
+  -webkit-background-clip: text;
+  -webkit-text-fill-color: transparent;
+  background-clip: text;
+  color: transparent;
+  font-size: 5rem;
+  margin-top: 0;
+  letter-spacing: 0.05em;
+  display: flex;
+  justify-content: center;
+}
+</style>
