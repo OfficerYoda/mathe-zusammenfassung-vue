e4f85b53e4a2cfa1a7ace880645ba98463fd7035
OfficerYoda <officeryoda@gmail.com>
2025-06-23 12:55:20 +0200

    Turns out I'm allowed can expose the api key

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,11 +1,19 @@
 import {initializeApp} from 'firebase/app';
 import {getFunctions, httpsCallable} from 'firebase/functions';
 
-// Import the private Firebase config
-import { firebaseConfig } from './firebaseConfig.private';
-
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(
+    {
+        apiKey: "AIzaSyBgE2Qza83zTYF76KyzU9WgBagDV-iBZPo", // It's ok to expose the firebase key
+        authDomain: "mathe-zusammenfassung.firebaseapp.com",
+        projectId: "mathe-zusammenfassung",
+        storageBucket: "mathe-zusammenfassung.firebasestorage.app",
+        messagingSenderId: "352927676071",
+        appId: "0:352927676072:web:bc7bbdfe219e5fa764f49d",
+        measurementId: "G-F8RNZHSD31"
+    }
+)
+
 const functions = getFunctions(app);
 
 // GitHub Issue Creation Function
